6189f543ef554bdab2a953bad7d4f9dc15be9deb
mitalee-verma <21BCS5651@cuchd.in>
2024-09-18 12:24:24 +0530

    minor changes

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/popularinwomen`)
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data));
   },[])
--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -18,7 +18,7 @@ const LoginSignup = () => {
   const login = async() =>{
     console.log("Login Function executed",formData);
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`,{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -39,7 +39,7 @@ const LoginSignup = () => {
   const signup = async() =>{
     console.log("Signup Function Executed",formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/signup`,{
       method:'POST',
       headers:{
         Accept:'application/form-data',
